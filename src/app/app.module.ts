6bc3c453d2963435db125ec8dfee868000537e29
Hitesh Tare <hitesh.tare@fractalink.com>
2017-11-21 15:11:25 +0530

    Changes: Redux functionality implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NgRedux, NgReduxModule } from '@angular-redux/store'
 
+import { AppComponent } from './app.component';
+import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 import { IAppState, INITIAL_STATE, rootReducer } from './store';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoOverviewComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    NgReduxModule
+    NgReduxModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
